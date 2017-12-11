7e7f7537a1e9bbc3cfc465ec50cbed89f358af0d
i320869 <pamela.piao@sap.com>
2017-12-11 13:44:55 +0800

    Config demo

---
+++ spring-cloud-config-client/src/test/java/com/neo/ApplicationTests.java
@@ -0,0 +1,23 @@
+//package com.neo;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Value;
+////import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+////@SpringBootTest
+//public class ApplicationTests {
+//
+//	@Value("${neo.hello}")
+//	private  String  hello;
+//
+//	@Test
+//	public void contextLoads() {
+//		System.out.println("************************************************************");
+//		System.out.println("hello value : "+hello);
+//		System.out.println("************************************************************");
+//	}
+//
+//}
